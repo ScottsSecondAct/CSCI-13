6d16b013f6879b16d0d418f5ff583ef2b81bc2a5
Scott Davis <scottdavis.gv@gmail.com>
2024-08-22 08:23:33 -0700

    Adding a more complete README.md file. Adding a program demonstrating Constructing and Traversing an Array.

---
+++ Arrays/ConstructingAndTraversingAnArray.java
@@ -0,0 +1,92 @@
+public class ConstructingAndTraversingAnArray {
+  public static void main(String[] args) {
+    // Declaring arrays
+    int[] integers = new int[5]; // Array of integers with a length of 5
+    double[] doubles = new double[10]; // Array of doubles with a length of 10
+    boolean[] booleans = new boolean[4]; // Array of booleans with a length of 4
+    char[] chars = new char[17]; // Array of characters with a length of 20
+    String[] strings = new String[3]; // Array of strings with a length of 3
+
+    // Assigning values to the integers array
+    integers[0] = 1;
+    integers[1] = 2;
+    integers[2] = 3;
+    integers[3] = 4;
+    integers[4] = 5;
+
+    // Assigning values to the doubles array
+    doubles[0] = 1.1;
+    doubles[1] = 2.2;
+    doubles[2] = 3.3;
+    doubles[3] = 4.4;
+    doubles[4] = 5.5;
+    doubles[5] = 6.6;
+    doubles[6] = 7.7;
+    doubles[7] = 8.8;
+    doubles[8] = 9.9;
+    doubles[9] = 10.10;
+
+    // Assigning values to the booleans array
+    booleans[0] = true;
+    booleans[1] = false;
+    booleans[2] = true;
+    booleans[3] = false;
+
+    chars[0] = 'H';
+    chars[1] = 'e';
+    chars[2] = 'l';
+    chars[3] = 'l';
+    chars[4] = 'o';
+    chars[5] = ',';
+    chars[6] = ' ';
+    chars[7] = 'J';
+    chars[8] = 'a';
+    chars[9] = 'v';
+    chars[10] = 'a';
+    chars[11] = ' ';
+    chars[12] = 'W';
+    chars[13] = 'o';
+    chars[14] = 'r';
+    chars[15] = 'l';
+    chars[16] = 'd';
+
+    // Assigning values to the strings array
+    strings[0] = "Hello";
+    strings[1] = "Java";
+    strings[2] = "World";
+
+    // Traversing the integers array
+    for (int i = 0; i < integers.length; i++) {
+      System.out.println("The element at index " + i + " is: " + integers[i]);
+    }
+    System.out.println();
+
+    // Traversing the doubles array
+    for (int i = 0; i < doubles.length; i++) {
+      System.out.println("The element at index " + i + " is: " + doubles[i]);
+    }
+    System.out.println();
+
+    // Traversing the booleans array
+    for (int i = 0; i < booleans.length; i++) {
+      System.out.println("The element at index " + i + " is: " + booleans[i]);
+    }
+    System.out.println();
+
+    // Traversing the chars array
+    for (int i = 0; i < chars.length; i++) {
+      System.out.println("The element at index " + i + " is: " + chars[i]);
+    }
+    System.out.println();
+
+    // Traversing the strings array
+    for (int i = 0; i < strings.length; i++) {
+      System.out.println("The element at index " + i + " is: " + strings[i]);
+    }
+    System.out.println();
+
+    // Out of bounds exceptions
+    // integers[5] = 6; // This will throw an ArrayIndexOutOfBoundsException
+    // integers[-1] = 0; // This will throw an ArrayIndexOutOfBoundsException
+  }
+}
--- Arrays/README.md
+++ Arrays/README.md
@@ -1,1 +1,42 @@
+# Java Arrays
 
+1. Array Basics
+
+    - Constructing and Traversing an Array.
+    - Accessing an Array.
+    - Initializing an Array.
+    - Random Access.
+    - Arrays and Methods.
+    - The For-Each Loop.
+    - The Array class.
+
+2. Array-Traversal Algorithms
+
+    - Printing an Array.
+    - Searching and Replacing.
+    - Testing for Equality.
+    - Reversing an Array.
+    - String Traversal Algorithms.
+    - Functional Approach.
+
+3. Reference Semantics
+
+    - Multiple Objects.
+
+4. Advanced Array Techniques
+
+    - Shifting Values in an Array.
+    - Array of Objects.
+    - Command-Line Arguments.
+
+5. Multidimensional Arrays
+
+    - Rectangular Two-Dimensional Arrays.
+    - Jagged Arrays.
+
+6. Arrays of Pixels
+
+7. Case Study: Benford's Law
+
+    - Tallying Values.
+    - Completing the Program.
