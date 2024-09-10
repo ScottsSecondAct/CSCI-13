7e611ef7a934ffa68d5de97974f6145f5396aea9
Scott Davis <scottdavis.gv@gmail.com>
2024-09-10 06:00:19 -0700

    Added LibraryBookManager as an example for a lab exercise.

---
+++ src/main/java/com/s1scottd/Week 4/Classroom Exercises/LibraryBookManager/Library.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Library {
+  private ArrayList<Book> books = new ArrayList<>();
+
+  public void addBook(Book book) {
+    books.add(book);
+  }
+
+  public void listBooks() {
+    for (Book book : books) {
+      System.out.println(book);
+    }
+  }
+
+  public void searchByTitle(String title) {
+    for (Book book : books) {
+      if (book.getTitle().equalsIgnoreCase(title)) {
+        System.out.println(book);
+        return;
+      }
+    }
+    System.out.println("Book not found.");
+  }
+}
