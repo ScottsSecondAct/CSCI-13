a02188249d6219e584a6f685fdecdcdc830a5aab
Scott Davis <scottdavis.gv@gmail.com>
2024-09-16 10:32:35 -0700

    Updated the Shape example.

--- src/main/java/com/s1scottd/Week 5/Inheritance/ShapeTest.java
+++ src/main/java/com/s1scottd/Week 5/Inheritance/ShapeTest.java
@@ -1,13 +1,22 @@
-package Inheritance;
+// Main class to test all the shapes
 public class ShapeTest {
   public static void main(String[] args) {
-    Shape circle = new Circle(5);
-    Shape rectangle = new Rectangle(4, 6);
+    // Creating a Circle object
+    Circle circle = new Circle("Blue", true, 5);
+    System.out.println(circle);
+    System.out.println("Circle Area: " + circle.area());
+    System.out.println("Circle Perimeter: " + circle.perimeter());
 
-    System.out.println("Circle Area: " + circle.area()); // Output: Circle Area: 78.53981633974483
-    System.out.println("Circle Perimeter: " + circle.perimeter()); // Output: Circle Perimeter: 31.41592653589793
+    // Creating a Rectangle object
+    Rectangle rectangle = new Rectangle("Green", false, 4, 6);
+    System.out.println(rectangle);
+    System.out.println("Rectangle Area: " + rectangle.area());
+    System.out.println("Rectangle Perimeter: " + rectangle.perimeter());
 
-    System.out.println("Rectangle Area: " + rectangle.area()); // Output: Rectangle Area: 24.0
-    System.out.println("Rectangle Perimeter: " + rectangle.perimeter()); // Output: Rectangle Perimeter: 20.0
+    // Creating a Triangle object
+    Triangle triangle = new Triangle("Red", true, 3, 4, 5);
+    System.out.println(triangle);
+    System.out.println("Triangle Area: " + triangle.area());
+    System.out.println("Triangle Perimeter: " + triangle.perimeter());
   }
 }
