cf387df914f1b0fc5f3a4751a444a5c6f431e97c
Scott Davis <scottdavis.gv@gmail.com>
2024-10-10 13:22:16 -0700

    Added a Copyright and License Header to each Java File

--- AddHeaderToAllJavaFiles.py
+++ AddHeaderToAllJavaFiles.py
@@ -44,7 +44,8 @@ header_text = """/**
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */"""
+ */
+ """
 
 # Specify the directory to start the search
 root_directory = "/Projects/CSCI-13"
