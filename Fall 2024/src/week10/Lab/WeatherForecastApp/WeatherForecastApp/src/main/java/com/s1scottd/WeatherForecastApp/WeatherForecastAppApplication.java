659d2a2880db6b4438d6ad016896ebd559b5cfbb
Scott Davis <scottdavis.gv@gmail.com>
2024-10-28 14:29:23 -0700

    Initial Commit of Week 10 Lab

---
+++ Fall 2024/src/week10/Lab/WeatherForecastApp/WeatherForecastApp/src/main/java/com/s1scottd/WeatherForecastApp/WeatherForecastAppApplication.java
@@ -0,0 +1,13 @@
+package com.s1scottd.WeatherForecastApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastAppApplication.class, args);
+	}
+
+}
---
+++ Fall 2024/src/week10/Lab/WeatherForecastApp/WeatherForecastApp/src/test/java/com/s1scottd/WeatherForecastApp/WeatherForecastAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1scottd.WeatherForecastApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
