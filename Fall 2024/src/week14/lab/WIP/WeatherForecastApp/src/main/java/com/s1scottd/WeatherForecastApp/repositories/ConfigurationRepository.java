1faf58778930f31d16e0810a0d727b02e9a7b5d0
Scott Davis <scottdavis.gv@gmail.com>
2024-11-22 05:40:26 -0800

    Removed WIP from Source Control

--- Fall 2024/src/week14/lab/WIP/WeatherForecastApp/src/main/java/com/s1scottd/WeatherForecastApp/repositories/ConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.s1scottd.WeatherForecastApp.repositories;
-
-import com.s1scottd.WeatherForecastApp.models.LastUsedStreetAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationRepository extends JpaRepository<LastUsedStreetAddress, String> {
-}
